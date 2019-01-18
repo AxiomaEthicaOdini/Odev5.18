75792c72e740464c4b640c766d4a2695b201e135
AxiomaEthicaOdini <46799882+AxiomaEthicaOdini@users.noreply.github.com>
2019-01-18 12:19:13 +0300

    Add files via upload

---
+++ Odev522.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("N\t10*N\t100*N\t1000*N");
+
+            for (int i = 1; i < 6; i++) 
+            {
+                Console.WriteLine("{0}\t{1}\t{2}\t{3}\t", i, i * 10, i * 100, i * 1000);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
